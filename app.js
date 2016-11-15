44af551a7b2d18f08f6207e761282fd803bad8f3
Florus Hoogendoorn <florus@ihoog.nl>
2016-11-15 23:28:47 +0100

    Commit

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var self = module.exports = {
 		
 		// when this app has been triggered on speech
 		Homey.manager('speech-input').on('speech', function( speech ){
+
+				Homey.log('onSpeech', speech);
 			
-			Homey.log('onSpeech', speech);
+				// say something back
+				speech.say( __("hello") );
 			
-			// say something back
-			speech.say( __("hello") );
+    //		});
 			
-		// });
-		
-		
-	
+    //    	console.log("trigger", trigger);			
+			
 			Homey.manager('ledring').animate('progress',
     
 				// optional animation-specific options
@@ -28,7 +28,7 @@ var self = module.exports = {
 				'INFORMATIVE',
     
 				// duration
-				90000,
+				60000,
     
 				// callback
 				function( err, success ) {
