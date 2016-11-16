fad341e6ae0c0bcf736da1cccd2a5849dffa27c2
Florus Hoogendoorn <florus@ihoog.nl>
2016-11-16 19:37:52 +0100

    Add some logging

--- app.js
+++ app.js
@@ -8,23 +8,29 @@ var self = module.exports = {
 		Homey.manager('speech-input').on('speech', function( speech ){
 
 			Homey.log('onSpeech', speech);
-
+			
 			var d = new Date();
 			var h = d.getHours();
+			
+			console.log("-> The hour is " + h);
 
 			if (h > 4 && h < 12) { 
 				speech.say( __("morning") );
+				console.log("-> if routine morning ... ");
 			}
 			if (h > 11 && h < 18) {
 				speech.say( __("afternoon") );
+				console.log("-> if routine afternoon ... ");
 			}
 			if (h > 17 && h < 23) {
 				speech.say( __("evening") );
+				console.log("-> if routine evening ... ");
 			}
 			if (h > 22 && h < 5) {
 				speech.say( __("night") );
+				console.log("-> if routine night ... ");
 			}
-			
+		
 			// say something back
 			speech.say( __("hello") );
 			
